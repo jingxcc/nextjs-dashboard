4c36f3f86109d4e14dd401bd5497ff08f5fcc54e
jingxcc <vera.xj.chen@gmail.com>
2025-12-29 00:14:50 +0800

    Add allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 export default nextConfig;
+
+module.exports = {
+  allowedDevOrigins: ["local-origin.dev", "*.local-origin.dev"],
+};
